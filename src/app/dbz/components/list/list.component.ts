61ac9fa30ed31d610586603bbbf2bc8b6b320a2b
jacorona <jacorona@aena.es>
2024-08-30 23:25:19 -0600

    inputs - outputs

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  @Output()
+  public onDeleteIndex: EventEmitter<number> = new EventEmitter();
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
+  onDeleteCharacter(index:number):void{
+    console.log(index)
+    this.onDeleteIndex.emit(index)
+  }
+}
